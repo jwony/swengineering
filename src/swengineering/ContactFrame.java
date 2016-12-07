a51f25cb5429e26c74db4ae62264d34ffb500f76
jwony <iyjw1118@naver.com>
2016-12-07 18:59:53 +0900

    클래스, 함수, 변수 대문자, 소문자 고침
    일단 이것부터 했어여...
    아직 다른 코드들과 머지하지 못해서 공통으로 쓰이는 함수들 더 수정 필요합니다.

--- src/swengineering/ContactFrame.java
+++ src/swengineering/ContactFrame.java
@@ -1,5 +1,234 @@
 package swengineering;
 
-public class ContactFrame {
+import java.awt.*;
+import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.StringTokenizer;
+import javax.swing.*;
 
+public class ContactFrame extends JFrame
+{
+	static String fileSrc="h:\\객체\\텀프로젝트\\CList.txt";
+	JLabel labelName, labelAddress, labelEmail, labelPhone, labelFind, labelTitle;
+	JTextField textName, textAddress, textEmail, textPhone, textFind;
+	JButton buttonFind, buttonSave, buttonRemove;
+	static String loginId;
+	static HashMap<String,ContactInfo> c = new HashMap<String,ContactInfo>(); 
+	
+	ContactFrame(String id)
+	{
+		loginId = id;
+		File fn = new File(fileSrc);
+		if(!fn.exists())
+		{
+			try{
+				FileWriter fw = new FileWriter(fn);
+				fw.close();
+			}
+			catch(Exception e){
+				e.printStackTrace();	// 무슨 오류인지 알고싶을 때
+				System.out.println("파일 입출력 에러");
+			}
+		}
+		loadData();
+		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container contentPane = getContentPane();
+		Font font1=new Font("고딕",Font.BOLD,30);
+		Font font2=new Font("고딕",Font.PLAIN,17);
+		Font font3=new Font("고딕",Font.PLAIN,14);
+		setTitle("주소록");
+		setSize(250,300);
+		setLayout(null);
+		
+		labelTitle = new JLabel();
+		labelName = new JLabel();
+		labelAddress = new JLabel();
+		labelEmail = new JLabel();
+		labelPhone = new JLabel();
+		labelFind = new JLabel();
+		textName = new JTextField();
+		textAddress = new JTextField();
+		textEmail = new JTextField();
+		textPhone = new JTextField();
+		textFind = new JTextField();
+		buttonFind = new JButton();
+		buttonSave = new JButton();
+		buttonRemove = new JButton();
+		
+		FrameManager.CreateJLabel(contentPane, labelTitle, "주소록", 100, 50, 80, 10, font1);
+		FrameManager.CreateJLabel(contentPane, labelFind, "이름 검색", 80, 20, 15, 65, font2);
+		FrameManager.CreateJLabel(contentPane, labelName, "이름", 80, 20, 35, 105, font2);
+		FrameManager.CreateJLabel(contentPane, labelAddress, "주소", 80, 20, 35, 130, font2);
+		FrameManager.CreateJLabel(contentPane, labelEmail, "이메일", 80, 20, 20, 155, font2);
+		FrameManager.CreateJLabel(contentPane, labelPhone, "전화번호", 80, 20, 10, 180, font2);
+		FrameManager.CreateJTextField(contentPane, textFind, "", 80, 20, 85, 65, font2);
+		FrameManager.CreateJTextField(contentPane, textName, "", 110, 20, 85, 105, font2);
+		FrameManager.CreateJTextField(contentPane, textAddress, "", 110, 20, 85, 130, font2);
+		FrameManager.CreateJTextField(contentPane, textEmail, "", 110, 20, 85, 155, font2);
+		FrameManager.CreateJTextField(contentPane, textPhone, "", 110, 20, 85, 180, font2);
+		FrameManager.CreateJButton(contentPane, buttonFind, "찾기", 55, 20, 170, 65, font3);
+		FrameManager.CreateJButton(contentPane, buttonSave, "저장", 70, 20, 40, 220, font2);
+		FrameManager.CreateJButton(contentPane, buttonRemove, "삭제", 70, 20, 130, 220, font2);
+		//textName.setEnabled(false);
+		//textAddress.setEnabled(false);
+		//textEmail.setEnabled(false);
+		//txtPhone.setEnabled(false);
+		
+		buttonFind.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				ContactInfo tmp = c.get(loginId+textFind.getText());
+				if(tmp != null)
+				{
+					//System.out.println(tmp.getId());
+					//System.out.println(loginId);
+					//if(tmp.getId().equals(loginId))
+					textName.setText(tmp.getName());
+					textAddress.setText(tmp.getAddress());
+					textEmail.setText(tmp.getEmail());
+					textPhone.setText(tmp.getPhone());
+				}
+				else if(tmp == null)
+					JOptionPane.showMessageDialog(null, "검색 결과가 없습니다.", "알림", JOptionPane.OK_OPTION);
+			}
+		});
+		
+		buttonSave.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				ContactInfo tmp = c.get(loginId+textName.getText());
+				if(tmp==null)	// 추가 완료
+				{
+					String name = textName.getText();
+					String add = textAddress.getText();
+					String email = textEmail.getText();
+					String phone = textPhone.getText();
+					
+					c.put(loginId+name, new ContactInfo(name,add,email,phone,loginId));
+					//System.out.println("데이터 추가 완료");
+					JOptionPane.showMessageDialog(null, "주소록 추가 완료!", "알림", JOptionPane.OK_OPTION);
+				}
+				else if(tmp != null)	// 수정 완료
+				{
+					String name = textName.getText();
+					String add = textAddress.getText();
+					String email = textEmail.getText();
+					String phone = textPhone.getText();
+					
+					tmp.setName(name);
+					tmp.setAddress(add);
+					tmp.setEmail(email);
+					tmp.setPhone(phone);
+					//System.out.println("데이터 수정 완료");
+					JOptionPane.showMessageDialog(null, "주소록 수정 완료!", "알림", JOptionPane.OK_OPTION);
+				}
+				
+				textName.setText("");
+				textAddress.setText("");
+				textEmail.setText("");
+				textPhone.setText("");
+				saveData();
+			}
+		});
+		
+		buttonRemove.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				int result = JOptionPane.showConfirmDialog(null, "삭제하시겠습니까?","경고",JOptionPane.YES_NO_OPTION);
+				if(result==JOptionPane.YES_OPTION)
+				{
+					c.remove(loginId+textName.getText());
+					JOptionPane.showMessageDialog(null, "삭제되었습니다.", "알림", JOptionPane.OK_OPTION);
+				}
+				textName.setText("");
+				textAddress.setText("");
+				textEmail.setText("");
+				textPhone.setText("");
+				//System.out.println("데이터 삭제 완료");
+				saveData();
+			}
+		});
+		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();		
+		int screenX=(int)(screen.getWidth() / 2 - this.getWidth() / 2);
+		int screenY=(int)(screen.getHeight() / 2 - this.getHeight() / 2);
+		setLocation(screenX,screenY);
+		setVisible(true);
+	}
+	
+	static void loadData()
+	{
+		FileReader fr=null;	// 일정이 저장된 파일을 읽기 위한 FileReader
+		BufferedReader br = null;	// 파일에서 문자열을 읽어오기 위한 BufferedReader
+		
+		String id=null,name=null,address=null,email=null,phone=null,r=null;	// bufferedreader에서 한 라인씩 읽어올 문자열 r
+		try
+		{
+			fr = new FileReader(fileSrc);	// 저장되어 있는 파일 오픈
+			br= new BufferedReader(fr);	// 파일의 문자열을 읽을 준비
+			while((r=br.readLine()) != null)	// 파일의 끝까지 한줄씩 읽는다.
+			{
+				StringTokenizer st = new StringTokenizer(r,",");	// 읽은 값을 토큰으로 나눠 저장
+				while(st.hasMoreTokens())
+				{
+					name = st.nextToken();
+					address = st.nextToken();
+					email = st.nextToken();
+					phone = st.nextToken();
+					id = st.nextToken();
+				} 
+				ContactFrame.c.put(id+name,new ContactInfo(name,address,email,phone,id));	// 객체를 만들어 저장
+			}
+			fr.close();	// 파일 닫기
+		}
+	
+		catch(Exception e)
+		{
+			e.printStackTrace();	// 무슨 오류인지 알고싶을 때
+			System.out.println("파일 입출력 에러");
+		}		
+	}
+	
+	static void saveData()
+	{
+		FileWriter fw=null;	// 파일라이터 
+		BufferedWriter bw=null;	// 버퍼라이터
+		String str;	// 파일에 저장할 문자열
+		try
+		{
+			fw = new FileWriter(fileSrc);	// 일정 파일 오픈
+			bw = new BufferedWriter(fw);	// 커서 넘기고
+			
+			Set<String> keys = ContactFrame.c.keySet();
+			Iterator<String> it = keys.iterator();
+			while(it.hasNext())
+			{
+				ContactInfo temp = ContactFrame.c.get(it.next());
+				String name=temp.getName();
+				String address=temp.getAddress();
+				String email=temp.getEmail();
+				String phone=temp.getPhone();
+				String id = temp.getId();
+				str=name+","+address+","+email+","+phone+","+id+"\r\n";
+				bw.write(str);
+			}
+			bw.close();	// 파일 닫기.
+			fw.close();
+		}
+		catch(IOException e)	// 예외처리
+		{
+			e.printStackTrace();
+			System.out.println("입출력 오류");
+		}
+	}
 }

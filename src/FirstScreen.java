8b46458e81e606749330e1b7ad63b40a3a475009
1111792 <skw62464@nate.com>
2016-12-07 18:35:01 +0900

    first commit
    firstScreen & mainScreen

---
+++ src/FirstScreen.java
@@ -0,0 +1,46 @@
+import java.awt.event.ActionEvent;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class FirstScreen extends PersonalAssistantSystem {
+	private JPanel loginPanel;
+	private JLabel id, pw;		
+	private JTextField txtId, txtPw;
+	private JButton btnLogin;
+	
+	public static void main(String[] args) {
+		FirstScreen setFirstScreen = new FirstScreen();
+		setFirstScreen.firstScreenPanel();
+	}
+	
+	public void firstScreenPanel() {	
+		loginPanel = new JPanel();
+		id = new JLabel("ID");	
+		pw = new JLabel("PASSWORD");
+		txtId = new JTextField(20);
+		txtPw = new JTextField(20);
+		btnLogin = new JButton("LOGIN");
+		loginPanel.add(id);
+		loginPanel.add(pw);
+		loginPanel.add(txtId);
+		loginPanel.add(txtPw);	
+		loginPanel.add(btnLogin);
+		btnLogin.addActionListener(this);
+		add(loginPanel);	
+		setSize(300, 400);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+		setVisible(true);
+		setResizable(false);
+	}
+
+	public void actionPerformed(ActionEvent e) {		
+		Object source = e.getSource();	
+		PersonalAssistantSystem mainScreen = new PersonalAssistantSystem();
+		if(source == btnLogin){
+			mainScreen.mainPanel();			
+		}		
+	}
+}
---
+++ src/PersonalAssistantSystem.java
@@ -0,0 +1,39 @@
+//import java.util.Comparator;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class PersonalAssistantSystem extends JFrame implements ActionListener {
+	private JPanel mainPanel;
+	private JButton btnAccount, btnPhoneBook, btnSchedule;	
+//	Comparator comparator;		
+	
+	public void mainPanel() {
+		mainPanel = new JPanel();
+		btnAccount = new JButton("Account");
+		btnPhoneBook = new JButton("PhoneBook");
+		btnSchedule = new JButton("Schedule");
+		btnAccount.addActionListener(this);
+		btnPhoneBook.addActionListener(this);
+		btnSchedule.addActionListener(this);
+		mainPanel.add(btnAccount);
+		mainPanel.add(btnPhoneBook);
+		mainPanel.add(btnSchedule);
+		add(mainPanel);
+		setTitle("S/E Project");
+		setSize(600, 400);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
+		setVisible(true);
+		setResizable(false);
+	}
+
+	public void actionPerformed(ActionEvent e) {		
+		Object source = e.getSource();
+		//PhoneBookManagement phoneBookScreen = new PhoneBookManagement();	
+		if(source == btnPhoneBook){	
+			//phoneBookScreen.phoneBookPanel();
+		}				
+	}
+}

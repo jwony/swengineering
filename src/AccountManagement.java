24c02be6872cbc199412b2f8ce19ff1c8512dc4c
HyeseokKim <hyeseok9302@naver.com>
2016-12-23 19:00:19 +0900

    Update AccountManagement.java

--- src/AccountManagement.java
+++ src/AccountManagement.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 
 public class AccountManagement extends PersonalAssistantSystem implements ActionListener{
 	private JPanel buttonPanel;
-	private JButton changeId, changePassword, logOut;
+	private JButton changeIdAndPassword, logOut;
 
 	public void accountFrame(){
 		setTitle("Account Management");
@@ -15,28 +15,23 @@ public class AccountManagement extends PersonalAssistantSystem implements Action
 		setResizable(false);
 		buttonPanel = new JPanel();
 		buttonPanel.setLayout(new GridLayout(3,1));		
-        changeId = new JButton("ID 변경");
-        changeId.addActionListener(this);
-        changePassword = new JButton("PASSWORD 변경");
-        changePassword.addActionListener(this);
-        logOut = new JButton("logOut");
-        logOut.addActionListener(this);
+               changeIdAndPassword = new JButton("Change Id And Password");
+               changeIdAndPassword.addActionListener(this);
+               logOut = new JButton("logOut");
+               logOut.addActionListener(this);
        
-        buttonPanel.add(changeId);
-        buttonPanel.add(changePassword);
-        buttonPanel.add(logOut);
-        add(buttonPanel);
+               buttonPanel.add(changeIdAndPassword);
+               buttonPanel.add(logOut);
+               add(buttonPanel);
 	}
 	
     public void actionPerformed(ActionEvent e){
     	Object source = e.getSource();
     	
-    	if(source == changeId){
+    	if(source == changeIdAndPassword){
     		
     	}
-    	if(source == changePassword){
-    		
-    	}
+    
     	else if(source == logOut){
             FirstScreen firstScreen = new FirstScreen();
             firstScreen.firstScreenPanel();
